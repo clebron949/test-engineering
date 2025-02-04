bc7e781e026d1a529932abe7b0ffe30571fab1d9
clebron949 <christopher.lebron1@gmail.com>
2025-02-03 21:36:20 -0500

    refactor dmm

--- src/components/equipments/dmm.tsx
+++ src/components/equipments/dmm.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-type MeasurementMode = "Voltage (V)" | "Current (A)" | "Resistance (Ω)";
+type MeasurementMode = "Voltage (V)" | "Current (A)" | "Ohms (Ω)";
 
 function Dmm() {
   const [displayValue, setDisplayValue] = useState<string>("0.00 V");
@@ -13,7 +13,7 @@ function Dmm() {
       case "Current (A)":
         setDisplayValue(`${(Math.random() * 5).toFixed(2)} A`);
         break;
-      case "Resistance (Ω)":
+      case "Ohms (Ω)":
         setDisplayValue(`${(Math.random() * 1000).toFixed(0)} Ω`);
         break;
       default:
@@ -24,12 +24,12 @@ function Dmm() {
   const buttons: MeasurementMode[] = [
     "Voltage (V)",
     "Current (A)",
-    "Resistance (Ω)",
+    "Ohms (Ω)",
   ];
 
   return (
     <main className="">
-      <div className="w-3xl bg-gray-200 rounded-lg p-5 relative shadow-lg">
+      <div className="w-[650px] bg-gray-200 rounded-lg p-5 relative shadow-lg">
         <p className="text-2xl text-start text-gray-700 font-medium font-mono">Digital Multimeter</p>
         <div className="grid grid-cols-3">
           <div className="col-span-2">
